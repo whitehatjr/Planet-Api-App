69fa31638c64daca3fc41f8903c5eaf1e958726c
vishalgaddam873 <vishal18@navgurukul.org>
2020-10-20 20:38:27 +0530

    Initial Comment

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./screens/Home";
+import DetailsScreen from "./screens/Details";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <AppContainer />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const appStackNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        headerShown: false
+      }
+    },
+    Details: {
+      screen: DetailsScreen
+    }
   },
-});
+  {
+    initialRouteName: "Home"
+  }
+);
+
+const AppContainer = createAppContainer(appStackNavigator);
